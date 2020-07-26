a3b5075bcfd80de476e2457709b3c9417e70ede3
dychw <25680873+dychw@users.noreply.github.com>
2020-07-26 12:48:55 +0900

    Add articles page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,21 +17,21 @@ Released   : 20140225
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-    <link href="css/default.css" rel="stylesheet" />
-    <link href="css/fonts.css" rel="stylesheet" />
+    <link href="/css/default.css" rel="stylesheet" />
+    <link href="/css/fonts.css" rel="stylesheet" />
 </head>
 <body>
     <div id="header-wrapper">
         <div id="header" class="container">
             <div id="logo">
-                <h1><a href="#">SimpleWork</a></h1>
+                <h1><a href="/">SimpleWork</a></h1>
             </div>
             <div id="menu">
                 <ul>
                     <li class="{{ Request::is('/') ? 'current_page_item' : '' }}"><a href="#" accesskey="1" title="">Homepage</a></li>
                     <li class="{{ Request::is('clients') ? 'current_page_item' : '' }}"><a href="#" accesskey="2" title="">Our Clients</a></li>
                     <li class="{{ Request::is('about') ? 'current_page_item' : '' }}"><a href="/about" accesskey="3" title="">About Us</a></li>
-                    <li class="{{ Request::is('careers') ? 'current_page_item' : '' }}"><a href="#" accesskey="4" title="">Careers</a></li>
+                    <li class="{{ Request::is('articles') ? 'current_page_item' : '' }}"><a href="/articles" accesskey="4" title="">Articles</a></li>
                     <li class="{{ Request::is('contact') ? 'current_page_item' : '' }}"><a href="#" accesskey="5" title="">Contact Us</a></li>
                 </ul>
             </div>
@@ -40,5 +40,9 @@ Released   : 20140225
     </div>
 
     @yield('content')
+
+    <div id="copyright" class="container">
+        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,10 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        'articles' => App\Article::take(3)->latest()->get()
+    ]);
 });
+
+Route::get('/articles', 'ArticlesController@index');
+Route::get('/articles/{article}', 'ArticlesController@show');
